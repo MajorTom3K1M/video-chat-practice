3a4d63699e0f501470cd0cfd5cccae4503037395
MajorTom3K1M <invaderzgame@gmail.com>
2019-03-16 02:13:30 +0700

    Add certs

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
+var fs = require('fs');
+
+var useAuth = true;
 var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
 var port = process.env.PORT || 3000;
+
+var options = {
+    key: fs.readFileSync('certs/server-key.pem'),
+    cert: fs.readFileSync('certs/server-crt.pem'),
+    ca: fs.readFileSync('certs/ca_client-crt.pem'),
+    //crl: fs.readFileSync('certs/ca-crl.pem'),
+};
+
+var server = http.createServer(options, app);
+var io = socketIO(server);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -16,6 +27,7 @@ io.sockets.on('error', e => console.log(e));
 app.get('*', function(req, res) {
     res.sendFile(`${__dirname}/public/index.html`);
 });
+
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
